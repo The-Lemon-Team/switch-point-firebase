973a78cde70f9756f1533ab37d06b3a82ba06b5c
Alexey Kartushin <alexey.kartushin@onsolve.com>
2022-02-12 03:38:17 +0300

    Reduce usage of FirebaseProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,29 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthScreenContainer } from './modules/auth/containers';
 import { PrivateRoute } from './modules/auth/containers';
 import { BoardContainer } from './modules/board';
-import { FirebaseProvider } from './modules/firebase/components/FirebaseProvider';
 
 export const App = () => {
   return (
     <Router>
-      <FirebaseProvider>
-        <Routes>
+      <Routes>
         <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <BoardContainer />
-              </PrivateRoute>
-            }
-          />
-          <Route path="/auth" element={<AuthScreenContainer />} />
-          <Route
-            path="/board"
-            element={
-              <PrivateRoute>
-                <BoardContainer />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </FirebaseProvider>
+          path="/"
+          element={
+            <PrivateRoute>
+              <BoardContainer />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/auth" element={<AuthScreenContainer />} />
+        <Route
+          path="/board"
+          element={
+            <PrivateRoute>
+              <BoardContainer />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 };
